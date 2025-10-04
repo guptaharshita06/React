ff6d5c8057000dc5b0acd99212da24df9ca45821
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-10-04 21:36:52 +0530

    Create App.jsx

---
+++ react-router-project/src/assets/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,  
+} from "react-router-dom";
+
+import Home from './components/Home';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+import Navbar from "./components/Navbar";
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: 
+      <div>
+        <Navbar/>
+        <Home/>
+      </div>
+    },
+    {
+      path: "/about",
+      element: 
+      <div>
+        <Navbar/>
+        <About/>
+      </div>
+    },
+    {
+      path: "/dashboard",
+      element: 
+      <div>
+        <Navbar/>
+        <Dashboard/>
+      </div>
+    },
+    {
+      path:"/student/:id",
+      element:
+      <div>
+        <Navbar/>
+        <ParamComp/>
+      </div>
+    }
+  ]
+);
+
+function App() {
+  
+
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default App
